f4caeeb3e3f75767634172078e5074f4ebf2261a
Timo Tuominen <tituomin@gmail.com>
2017-05-16 20:51:58 +0300

    Fix timeout.

--- settings.py
+++ settings.py
@@ -5,4 +5,4 @@ load_dotenv(find_dotenv())
 
 SERVICEMAP_URL = os.environ.get('SERVICEMAP_URL', 'http://localhost:9001')
 BROWSER = os.environ.get('BROWSER', 'Chrome')
-TIMEOUT = os.environ.get('TIMEOUT', 1)
+TIMEOUT = int(os.environ.get('TIMEOUT', 10))
