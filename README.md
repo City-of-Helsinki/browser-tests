9cfc4a0a79757705baf0c95396dd1d40fcc0284d
Tomi Hukkalainen <tomi.hukkalainen@iki.fi>
2017-10-18 13:19:43 +0300

    Add option for running on SauceLabs

--- README.md
+++ README.md
@@ -6,14 +6,18 @@ pip install -r requirements.txt
 ```
 
 ## Config
-Setup .env or your regular env
+Setup .env or your regular env:
 
 ```
+SAUCELABS_USERNAME
+SAUCELABS_ACCESS_KEY
 HELSINKI_ADFS_USER
 HELSINKI_ADFS_PASSWORD
 FACEBOOK_USER
 FACEBOOK_PASSWORD
 ```
+
+(HELSINKI_ADFS parameters are not currently used)
 
 and optionally change defaults for
 
@@ -24,6 +28,13 @@ TIMEOUT
 SERVICEMAP_URL
 VARAAMO_URL
 ```
+
+If SAUCELABS_USERNAME and ACCESS_KEY are set,
+BROWSER can be `ie11` for Internet Explorer 11 on Windows 10,
+`ie10` for Internet Explorer 10 on Windows 7,
+or anything else from Chrome on OS X.
+
+Otherwise browser is always local Chrome.
 
 ## Run
 
--- fixtures/common.py
+++ fixtures/common.py
@@ -4,15 +4,37 @@ import settings
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 
-
 @pytest.fixture(scope='session')
 def driver():
-    chrome_options = Options()
-    if settings.HEADLESS:
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--window-size=512,512')
-    driver = webdriver.Chrome(chrome_options=chrome_options)
+    if settings.SAUCELABS_USERNAME:
+        if settings.BROWSER == 'ie11':
+            desired_cap = {
+                'platform': "Windows 10",
+                'browserName': "internet explorer",
+                'version': "11.103",
+                }
+        elif settings.BROWSER == 'ie10':
+            desired_cap = {
+                'platform': "Windows 7",
+                'browserName': "internet explorer",
+                'version': "10.0",
+                }
+        else:
+            desired_cap = {
+                    'platform': "Mac OS X 10.9",
+                    'browserName': "chrome",
+                    'version': "31",
+                    }
+        driver = webdriver.Remote(
+           command_executor=f'http://{settings.SAUCELABS_USERNAME}:{settings.SAUCELABS_ACCESS_KEY}@ondemand.saucelabs.com:80/wd/hub',
+           desired_capabilities=desired_cap)
+    else:
+        chrome_options = Options()
+        if settings.HEADLESS:
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--window-size=512,512')
+        driver = webdriver.Chrome(chrome_options=chrome_options)
     yield driver
     driver.quit()
 
